604a36e089e67a619cee68fbe8492f86177b5543
HectorV <hectorfabiovasquez@gmail.com>
2022-11-21 10:34:15 -0500

    Finished Aplicación Tareas

--- 06-Tareas/aplicacion-tareas/src/componentes/ListaTareas.jsx
+++ 06-Tareas/aplicacion-tareas/src/componentes/ListaTareas.jsx
@@ -13,7 +13,22 @@ function ListaTareas() {
       const tareasActualizadas = [tarea, ...tareas];
       setTareas(tareasActualizadas);
     }
-  }
+  };
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
+
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if(tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  };
 
   return (
     <>
@@ -21,9 +36,13 @@ function ListaTareas() {
       <div className='tareas-lista-contenedor'>
         {
           tareas.map((tarea) => 
-          <Tarea 
+          <Tarea
+            key={tarea.id}
+            id={tarea.id}
             texto={tarea.texto}
             completada={tarea.completada}
+            eliminarTarea={eliminarTarea}
+            completarTarea={completarTarea}
           />
           )
         }
